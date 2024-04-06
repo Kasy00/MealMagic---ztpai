15c8250035604150fef7204a44dd0b9edf8626df
Kasy00 <kamilsyktus3@gmail.com>
2024-04-06 11:16:30 +0200

    Created basic backend structure

---
+++ src/test/java/com/MealMagic/MealMagicApp/MealMagicAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.MealMagic.MealMagicApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MealMagicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
